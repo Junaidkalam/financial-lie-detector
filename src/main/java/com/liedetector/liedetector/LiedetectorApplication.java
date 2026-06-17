68827934efa985cfcada38198273b53bde1212f2
Junaid Kalam <junaidkalam02@gmail.com>
2026-06-17 17:44:24 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/liedetector/liedetector/LiedetectorApplication.java
@@ -0,0 +1,13 @@
+package com.liedetector.liedetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiedetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiedetectorApplication.class, args);
+	}
+
+}
